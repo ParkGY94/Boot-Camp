bcea2728859647125748b0c0690d3ea770489cca
ParkGY94 <59813824+ParkGY94@users.noreply.github.com>
2020-02-06 20:49:51 +0900

    Add files via upload

---
+++ settingweb/src/main/java/com/devfun/vo/MovieVO.java
@@ -0,0 +1,28 @@
+package com.devfun.vo;
+
+public class MovieVO {
+	private String movie_name;
+	private String director;
+	private String types;
+	
+	public String getmovie_name() {
+		return movie_name;
+	}
+	public void setmovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+	
+	public String getdirector() {
+		return director;
+	}
+	public void setdirector(String director) {
+		this.director = director;
+	}
+	
+	public String gettypes() {
+		return types;
+	}
+	public void settypes(String types) {
+		this.types = types;
+	}
+};
